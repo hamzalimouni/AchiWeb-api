fb2482d5726b77097238cfa3f67c432ba9bbbe65
ELYAMOUNI <hamzalimouni9@gmail.com>
2023-03-29 00:03:38 +0200

    update

--- index.js
+++ index.js
@@ -10,12 +10,29 @@ const skillRoute = require("./routes/skills");
 const studentSkillRoute = require("./routes/students_skills");
 const projectSkillRoute = require("./routes/project_skills");
 const enrollementRoute = require("./routes/enrollements");
+const multer = require('multer');
 
 app.use(cors());
 app.use(express.json());
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URL).then(console.log("Connected to Mongo db")).catch((err) => console.log(err));
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, '../frontend/src/upload')
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + file.originalname)
+    }
+})
+
+const upload = multer({ storage })
+
+app.post('/api/upload', upload.single('file'), function (req, res) {
+    const file = req.file;
+    res.status(200).json(file.filename);
+})
 
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
